75a3fbc4ae6fcaa6dd4129b4c61fe841595d84db
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-05-27 16:09:40 +0200

    addition of balance

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -5,7 +5,17 @@ const TotalBalanceBox = ({
   totalBanks,
   totalCurrentBalance,
 }: TotalBalanceBoxProps) => {
-  return <div>TotalBalanceBox</div>;
+  return (
+    <section className="total-balance">
+      <div className="total-balance-chart">{/* Doughnut chart */}</div>
+      <div className="flex flex-col gap-6">
+        <h2 className="header-2">Bank Account: {totalBanks} </h2>
+      </div>
+      <div className="flex flex-col gap-2">
+        <p className="total-balance-label">Total Current Balance</p>
+      </div>
+    </section>
+  );
 };
 
 export default TotalBalanceBox;
