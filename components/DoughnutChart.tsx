dbc07a31d76a65ddc59db037eed5d5b347ab299a
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-05-27 23:46:21 +0200

    addtion of functionality

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -5,12 +5,19 @@ import { Doughnut } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const DoughnutChart = () => {
-  return (
-    <div>
-      <Doughnut data={[]} />
-    </div>
-  );
+const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
+  const data = {
+    dataset: [
+      {
+        label: "Banks",
+        data: [1250, 2340, 3746],
+        backgroundColor: ["#0747b6", "#2265d8", "#2f91fa"],
+      },
+    ],
+    labels: ["Bank 1", "Bank 2", "Bank 3"],
+  };
+
+  return <Doughnut data={[]} />;
 };
 
 export default DoughnutChart;
