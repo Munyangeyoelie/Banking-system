642f692d93e6daa36f0914ddfa09734da7a6e3fd
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-05-27 15:22:08 +0200

    editing home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+const loggedIn = { firstName: "Munyangeyo" };
 
 const Home = () => {
   return (
     <section className="home">
       <div className="home-content">
-        <header className="home-header">Welcome to your first bank</header>
+        <header className="home-header">
+          <HeaderBox
+            type="gretting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+          />
+        </header>
       </div>
     </section>
   );
---
+++ components/HeaderBox.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HeaderBox = () => {
+  return <div>Welcome bro </div>;
+};
+
+export default HeaderBox;
