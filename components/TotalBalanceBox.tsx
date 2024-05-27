28870a6a7c079489ee367ef6162fc27861b34483
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-05-27 17:03:21 +0200

    addtion of functionality

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,4 +1,6 @@
+import { formatAmount } from "@/lib/utils";
 import React from "react";
+import AnimatedCounter from "./AnimatedCounter";
 
 const TotalBalanceBox = ({
   accounts = [],
@@ -10,9 +12,13 @@ const TotalBalanceBox = ({
       <div className="total-balance-chart">{/* Doughnut chart */}</div>
       <div className="flex flex-col gap-6">
         <h2 className="header-2">Bank Account: {totalBanks} </h2>
-      </div>
-      <div className="flex flex-col gap-2">
-        <p className="total-balance-label">Total Current Balance</p>
+
+        <div className="flex flex-col gap-2">
+          <p className="total-balance-label">Total Current Balance</p>
+          <p className="total-balance-amount flex-center gap-2">
+            <AnimatedCounter amount={totalCurrentBalance} />
+          </p>
+        </div>
       </div>
     </section>
   );
