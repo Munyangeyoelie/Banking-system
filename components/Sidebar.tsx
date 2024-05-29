3f87c6161bdcb5d7216a3fcd34294499d4634655
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-05-29 15:57:26 +0200

    changes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 const Sidebar = ({ user }: SiderbarProps) => {
+  const pathname = usePathname();
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
@@ -16,6 +20,19 @@ const Sidebar = ({ user }: SiderbarProps) => {
           />
           <h1 className="sidebar-logo">Ngeyo Banking</h1>
         </Link>
+        {sidebarLinks.map((item) => {
+          const isActive =
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
+          return (
+            <Link
+              href={item.route}
+              key={item.label}
+              className={cn(sidebarLinks)}
+            >
+              {item.label}
+            </Link>
+          );
+        })}
       </nav>
     </section>
   );
