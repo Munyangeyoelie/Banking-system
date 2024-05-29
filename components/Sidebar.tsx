74d906fb847a74bc61d21da1cff3b7ba6c557e6c
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-05-29 15:41:32 +0200

    sidebar Image

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 const Sidebar = ({ user }: SiderbarProps) => {
-  return <section className="sidebar"></section>;
+  return (
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 cursor-pointer items-center gap-2 ">
+          <Image
+            src="/Icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Company Logo"
+            className="size-[24px]max-xl:size-14"
+          />
+        </Link>
+      </nav>
+    </section>
+  );
 };
 
 export default Sidebar;
